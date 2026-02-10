c38b29e303af23cd30b01f0f81d9395f96116ffd
LeRFID <abantaojm56@gmail.com>
2026-02-10 13:46:20 +0800

    CP2_Homework_2

---
+++ CP2_HW2.py
@@ -0,0 +1,15 @@
+name = 'Johnmarc'
+print ("Full name: Johnmarc C. Abantao")
+print (type (name)) 
+
+age = 18
+print ("\nAge: 22")
+print (type (age))
+
+height = 1.74
+print ("\nHeight: 1.74 m")
+print (type (height))
+
+is_working = False
+print ("\nCurrently Working? False")
+print (type (is_working))
